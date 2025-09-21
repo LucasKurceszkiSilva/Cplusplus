4b565ca720487a11d648872248ff508bbb26e170
Lucasdodia16 <lucas.kurceszki.silva@gmail.com>
2025-09-21 11:16:56 -0300

    Update lasers.cpp

--- Codeforces/contest1050/lasers.cpp
+++ Codeforces/contest1050/lasers.cpp
@@ -1,13 +1,28 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    //sync_with_stdio(false);
+int main() {
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int a; cin >> a;
-    for(int i=0;i<a;i++){
-        
+
+    int t; 
+    cin >> t;
+    while (t--){
+        int n, m, x, y;
+        cin >> n >> m >> x >> y;
+
+        int cross = 0, temp;
+
+        for (int i = 0; i < n; i++) {
+            cin >> temp;
+            if (temp < y) cross++;
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> temp;
+            if (temp < x) cross++;
+        }
+
+        cout << cross << endl;
     }
     return 0;
 }
